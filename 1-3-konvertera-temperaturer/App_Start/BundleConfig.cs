62ea244f7ea04cdb5d8113c14c795655582fb7be
Johnny Pesola <johnny@pesola.eu>
2015-01-29 14:38:21 +0100

    First commit

---
+++ 1-3-konvertera-temperaturer/App_Start/BundleConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace _1_3_konvertera_temperaturer
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+
+            bundles.Add(new ScriptBundle("~/Content/javascript").Include(
+                "~/Scripts/main.js"
+            ));
+
+            bundles.Add(new StyleBundle("~/Content/styles").Include(
+                "~/Content/css/style.css"
+            ));
+
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
---
+++ 1-3-konvertera-temperaturer/Model/TemperatureConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _1_3_konvertera_temperaturer.Model
+{
+    public static class TemperatureConverter
+    {
+        public static int  CelciusToFahrenheit(int degreesC)
+        {
+            return (int)Math.Round(degreesC * 1.8 + 32);
+        }
+
+        public static int FahrenheitToCelcius(int degreesF)
+        {
+            return (int) (degreesF - 32) * 5 / 9;
+        }
+    }
+}
